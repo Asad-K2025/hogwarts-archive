ace1a7d8076d9ac6c8eb57e56f131bba699043c4
Asad Khalid <akha0107@uni.sydney.edu.au>
2026-01-15 12:14:50 +1100

    Uplaoded javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Archive"},{"p":"<Unnamed>","l":"CommandHandler"},{"p":"<Unnamed>","l":"CommonErrorChecker"},{"p":"<Unnamed>","l":"SpellBook"},{"p":"<Unnamed>","l":"Student"}];updateSearchResults();
